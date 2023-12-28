c4cdde29655dc0046517b26d2084c10351d5f6e9
yumotoyasumasa0225 <yumoto41104@d6.dion.ne.jp>
2023-12-28 20:54:11 +0900

    5 commit

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -9,29 +9,27 @@
     <p>
         <?php
 // ソートする配列を宣言
-            function sort_2way($order,$array) {
+            function sort_2way($array,$order) {
                 if ($order == true) {
                     echo "昇順にソートします\n";
                     echo '<br>';
                     sort($array);
-                    } else {
+                } else {
                     echo "降順にソートします\n";
 		            echo '<br>';
                     rsort($array);
                 }
-                foreach ($array as $key => $val) {
+                foreach ($array as $val) {
                     echo "$val\n";
                     echo '<br>';
                 }
             }
 //配列
-$array = [15, 4, 18, 23, 10 ];
+$nums = [15, 4, 18, 23, 10 ];
 // 関数を呼び出す（1回目）
-$order = true;
-sort_2way($order,$array);
+sort_2way($nums,true);
 // 関数を呼び出す（2回目）
-$order = false;
-sort_2way($order,$array);
+sort_2way($nums,false);
         ?>
     </p>
 </body>
